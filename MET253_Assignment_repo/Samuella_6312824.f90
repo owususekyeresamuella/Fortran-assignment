5c0f25bbdb1ddbac63d57e9b7bdda88289e20743
owususekyeresamuella <owususekyeresamuella@gmail.com>
2026-05-02 00:37:32 +0000

    Update Samuella_6312824.f90
    DO , loops through the list of 10 students one by one 
    IF , checks if a student got an A
    ELSE IF, checks if a student got a B or C  if they didnt get an A
    ELSE, it automatically assigns an F to anyone who could not reach 40

--- MET253_Assignment_repo/Samuella_6312824.f90
+++ MET253_Assignment_repo/Samuella_6312824.f90
@@ -1,23 +1,27 @@
-PROGRAM Student_score
-IMPLICIT NONE
-REAL DIMENSION(10)::X
-INTERGER DIMENSION(10)::i
-PRINT*,enter 10 scores
-IF X= 80>=100
-PRINT*, 'grade A'AND 'DISTINCTION'
-END IF
-ELSE X=(60>=79)
-PRINT*, 'grade B'AND 'Credit'
-END ELSE
-ELSE IF X=(40>=59)
-PRINT*, 'grade C' AND 'Pass'
-If x=(0>=39)
-PRINT*,'grade F' AND 'Fail'
-DO i(1,10)
-PRINT*,(1,10)
-END DO
-END PROGRAM Student_score
-!samuella owusu sekyere
-!6312824
-
-
+ PROGRAM Student_score
+ IMPLICIT NONE
+ INTEGER, DIMENSION(10) :: scores
+ INTEGER :: i
+ CHARACTER(LEN=1)  :: grade
+ CHARACTER(LEN=12) :: remark
+ DATA scores /85, 62, 45, 91, 38, 74, 55, 88, 61, 47/
+ PRINT *, "Score" ,"Grade" ,  "Remark"
+ DO i = 1, 10
+ IF (scores(i) >= 80) THEN
+ grade = 'A'
+ remark = 'DISTINCTION'
+ ELSE IF (scores(i) >= 60) THEN
+ grade = 'B'
+ remark = 'Good'
+ ELSE IF (scores(i) >= 40) THEN
+ grade = 'C'
+ remark = 'Credit'
+ ELSE
+ grade = 'F'
+ remark = 'Fail'
+ END IF
+ PRINT "(I5, T10, A1, T15, A12)", Scores(i) , Grade, Remark
+ END DO
+ END PROGRAM Student_score
+ !Samuella owusu sekyere
+ !6312824
